a84607100e44a94327cff5de5963695890ab2dc7
Vladislav Shipovskoi <sgipovskoi@gmail.com>
2018-02-24 22:18:36 +0700

    change codestyle

--- kinopoisk/spiders/kinopoisk_spider.py
+++ kinopoisk/spiders/kinopoisk_spider.py
@@ -4,7 +4,8 @@ from scrapy.spiders import CrawlSpider
 from kinopoisk.items import KinopoiskItem
 
 
-BASE_URL = 'https://www.kinopoisk.ru/top/navigator/m_act%5Byears%5D/1890:2017/m_act%5Brating%5D/1:/order/rating/page/{}/#results'
+BASE_URL = 'https://www.kinopoisk.ru/top/navigator/m_act%5Byears%5D/1890:201' \
+           '7/m_act%5Brating%5D/1:/order/rating/page/{}/#results'
 SENTIMENT_DICT = {
     'response good': 'good',
     'response': 'neutral',
@@ -25,31 +26,62 @@ class FilmsSpider(CrawlSpider):
     page = 1
 
     def parse(self, response):
-        all_url = response.xpath('//div[@class = "info"]/div[@class="name"]/a/@href').extract()
+        all_url = response.xpath(
+            '//div[@class = "info"]/div[@class="name"]/a/@href'
+        ).extract()
         if all_url:
             for url in all_url:
-                yield scrapy.Request(response.urljoin(url),callback=self.parse_movies)
+                yield scrapy.Request(
+                    response.urljoin(url),
+                    callback=self.parse_movies
+                )
 
             self.page += 1
-            yield scrapy.Request(BASE_URL.format(self.page), callback=self.parse)
+            yield scrapy.Request(
+                BASE_URL.format(self.page),
+                callback=self.parse
+            )
 
     def parse_movies(self, response):
-        reviews_url = response.xpath('//li[@class="all"]/a/@href').extract_first()
+        reviews_url = response.xpath(
+            '//li[@class="all"]/a/@href'
+        ).extract_first()
         if reviews_url:
-            yield scrapy.Request(response.urljoin(reviews_url), callback=self.parse_reviews)
+            yield scrapy.Request(
+                response.urljoin(reviews_url),
+                callback=self.parse_reviews
+            )
 
     def parse_reviews(self, response):
-        reviews = response.xpath('//div[@class="reviewItem userReview"]//div[@itemprop="reviews"]')
+        reviews = response.xpath(
+            '//div[@class="reviewItem userReview"]//div[@itemprop="reviews"]'
+        )
         if reviews:
             for review in reviews:
                 review_item = KinopoiskItem()
-                review_item['sentiment'] = SENTIMENT_DICT[review.xpath('@class').extract_first()]
-                review_item['text'] = ' '.join((x.strip() for x in review.xpath('table//text()').extract() if x.strip()))
-                review_item['author'] = review.xpath('//div[@itemprop="author"]//a/text()').extract_first()
-                review_item['author_link'] = review.xpath('//div[@itemprop="author"]//a/@href').extract_first()
+                review_item['sentiment'] = SENTIMENT_DICT[review.xpath(
+                    '@class'
+                ).extract_first()]
+
+                review_item['text'] = ' '.join(
+                    (
+                        x.strip()
+                        for x in review.xpath('table//text()'
+                                              ).extract() if x.strip()))
+
+                review_item['author'] = review.xpath(
+                    '//div[@itemprop="author"]//a/text()'
+                ).extract_first()
+
+                review_item['author_link'] = review.xpath(
+                    '//div[@itemprop="author"]//a/@href'
+                ).extract_first()
 
                 yield review_item
 
             next_page = response.xpath('//a[text()="»"]/@href').extract_first()
             if next_page:
-                yield scrapy.Request(response.urljoin(next_page), callback=self.parse_reviews)
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    callback=self.parse_reviews
+                )
